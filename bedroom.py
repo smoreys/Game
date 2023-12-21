24117a8489495a1f7ecbc15c7fbcec2dccb7aa67
smoreys <150430670+smoreys@users.noreply.github.com>
2023-12-21 15:16:34 +0700

    Update bedroom.py
    Сделала норм визуал

--- bedroom.py
+++ bedroom.py
@@ -7,7 +7,7 @@ class Board:
         self.height = height
         self.board = [[0] * width for _ in range(height)]
         self.left = 150
-        self.top = 250
+        self.top = 300
         self.cell_size = 60
         self.room_locked = True #дверь открыта - False, дверь закрыта - True, если открыта, то уровень пройден
         self.box_close = True
@@ -20,39 +20,35 @@ class Board:
     def render(self, screen):
         # x - код цвета в зависимости от того, открыта дверь или нет
         if self.room_locked:
-            x = pygame.Color(100, 255, 100)
+            x = pygame.Color(220, 20, 60)
             # рисуем дверь закрытую
-            pygame.draw.rect(screen, x, (self.left + 200, self.top - 120, 80, 120), 5)
+            door_closed.draw(screen)
         else:
-            x = pygame.Color(0, 170, 255)
+            x = pygame.Color(250, 128, 114)
             # рисуем дверь открытую
-            pygame.draw.rect(screen, x, (self.left + 200, self.top - 120, 80, 120), 5)
-            pygame.draw.line(screen, x, (self.left + 200, self.top - 120), (self.left + 220, self.top - 80), 5)
-            pygame.draw.line(screen, x, (self.left + 200, self.top), (self.left + 220, self.top - 20), 5)
-            pygame.draw.line(screen, x, (self.left + 220, self.top - 20), (self.left + 220, self.top - 80), 5)
+            door_open.draw(screen)
+        pygame.draw.rect(screen, pygame.Color(230, 100, 92), (self.left, self.top, 8 * self.cell_size, 6 * self.cell_size))
         for i in range(self.height):  #рисуем поле
             for j in range(self.width):
                 pygame.draw.rect(screen, x, (self.cell_size * j + self.left,
                                                                       self.cell_size * i + self.top, self.cell_size,
                                                                       self.cell_size), 2)
-
+        #текст на картине + сама картинаs
         pygame.draw.rect(screen, x, (self.left, self.top - 170, 480, 170), 4) #стена с дверью
-        # рисуем кровать
-        pygame.draw.rect(screen, pygame.Color(255, 0, 255), (6 * self.cell_size + self.left, self.top, 2 * self.cell_size, 3 * self.cell_size))
-        pygame.draw.rect(screen, pygame.Color(200, 0, 200), (6 * self.cell_size + self.left, self.top + 150, 2 * self.cell_size, 0.5 * self.cell_size))
-        pygame.draw.rect(screen, pygame.Color(255, 150, 255), (6 * self.cell_size + self.left + 20, self.top + 20, 2 * self.cell_size - 40, 0.5 * self.cell_size))
-        #рисуем стол с компом и стулом
-        pygame.draw.rect(screen, pygame.Color(200, 200, 255), (self.cell_size + self.left, self.top + self.cell_size * 5 - 2, 120, 60))
-        pygame.draw.rect(screen, pygame.Color(200, 0, 200), (1.5 * self.cell_size + self.left + 2, self.top + self.cell_size * 5.25 - 2, 50, 30))
-        pygame.draw.circle(screen, pygame.Color(0, 200, 150), (2 * self.cell_size + self.left - 10, self.top + self.cell_size * 5 - 30), 25)
-        # ща буит магия и появится коробка с секретиками:)
-        pygame.draw.rect(screen, pygame.Color(255, 222, 173), (self.left + 2.5, self.top + 1, 120, 60))
-        pygame.draw.rect(screen, pygame.Color(222, 184, 135), (self.left + 2.5, self.top + 1 + 40, 120, 20))
-        if self.box_close: #чекаем открыта коробка или закрыта
-            pygame.draw.line(screen, pygame.Color(222, 184, 135), (self.left + 2.5, self.top + 1 + 20), (self.left + 2.5 + 119, self.top + 1 + 20), 2)
-        else:
-            pygame.draw.rect(screen, pygame.Color(222, 184, 135), (self.left + 2.5, self.top + 1, 120, 60))
-            pygame.draw.rect(screen, pygame.Color(159, 130, 90), (self.left + 2.5 + 10, self.top + 1 + 20, 100, 20))
+        pygame.draw.rect(screen, pygame.Color(255, 182, 193), (self.left + 5.5 * self.cell_size, 150, 130, 72))
+        pygame.draw.rect(screen, pygame.Color(219, 112, 147), (self.left + 5.5 * self.cell_size, 150, 130, 72), 5)
+        font = pygame.font.Font(None, 35)
+        line1 = font.render("10010010", True, (200, 255, 100))
+        lx1 = self.left + 5.5 * self.cell_size + 10
+        ly1 = 155
+        lh = 15
+        lw = 15
+        screen.blit(line1, (lx1, ly1))
+        line2 = font.render("00101001", True, (200, 255, 250))
+        screen.blit(line2, (lx1, ly1 + 20))
+        line3 = font.render("10100101", True, (250, 255, 100))
+        screen.blit(line3, (lx1, ly1 + 40))
+        #магия отменяется, дев нашел спрайты
 
 
 class Hero:
@@ -89,10 +85,10 @@ class Hero:
             self.up = 0
         else:
             self.up += 1
-        if y - 5 > self.top - 110:
+        if y - 5 > self.top - 50:
             return [x, y - 5]
         return [x, y]
-
+#переписать движение в одну функцию
     def moving_down(self, x, y):
         self.direction = 'down'
         sprite.image = load_image(self.animation['down'][self.down])
@@ -100,7 +96,7 @@ class Hero:
             self.down = 0
         else:
             self.down += 1
-        if y <= self.left + self.cell_size * 8 - 10:
+        if y <= self.top + self.cell_size * 5 - 30:
             return [x, y + 5]
         return [x, y]
 
@@ -129,8 +125,10 @@ class Hero:
 def load_image(name, colorkey=None):
     fullname = os.path.join('animation', name)
     if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
+        fullname = os.path.join(name)
+        if not os.path.isfile(fullname):
+            print(f"Файл с изображением '{fullname}' не найден")
+            sys.exit()
     image = pygame.image.load(fullname)
     if colorkey is not None:
         image = image.convert()
@@ -146,12 +144,49 @@ size = width, height = 800, 800
 screen = pygame.display.set_mode(size)
 board = Board(8, 6)
 all_sprites = pygame.sprite.Group()
+bed = pygame.sprite.Sprite()
+bed.image = load_image('bed.png', (32, 30, 41))
+bed.image = pygame.transform.scale(bed.image, (300, 300))
+bed.rect = bed.image.get_rect()
+all_sprites.add(bed)
+bed.rect = [380, 200]
+box_sp = pygame.sprite.Sprite()
+box_sp.image = load_image("box-c.png")
+box_sp.image = pygame.transform.scale(box_sp.image, (150, 120))
+box_sp.rect = box_sp.image.get_rect()
+all_sprites.add(box_sp)
+box_sp.rect = [130, 250]
 sprite = pygame.sprite.Sprite()
 sprite.image = load_image("down-stand.png")
 sprite.rect = sprite.image.get_rect()
+sprite.rect = [300, 300]
 all_sprites.add(sprite)
-sprite.rect = [200, 200]
-screen.fill((255, 255, 255))
+door_open = pygame.sprite.Group()
+d_open = pygame.sprite.Sprite()
+d_open.image = load_image("open-door.png", (166, 172, 186))
+d_open.image = pygame.transform.scale(d_open.image, (120, 170))
+d_open.rect = d_open.image.get_rect()
+d_open.rect = [350, 135]
+door_open.add(d_open)
+door_closed = pygame.sprite.Group()
+d_closed = pygame.sprite.Sprite()
+d_closed.image = load_image('closed-door.png', (76, 80, 86))
+d_closed.image = pygame.transform.scale(d_closed.image, (90, 150))
+d_closed.rect = d_closed.image.get_rect()
+d_closed.rect = [350, 150]
+door_closed.add(d_closed)
+plant = pygame.sprite.Sprite()
+plant.image = load_image('plant.png', (0, 0, 0))
+plant.image = pygame.transform.scale(plant.image, (80, 120))
+plant.rect = plant.image.get_rect()
+plant.rect = [553, 539]
+all_sprites.add(plant)
+table = pygame.sprite.Sprite()
+table.image = load_image('table.png', (0, 0, 0))
+table.image = pygame.transform.scale(table.image, (180, 130))
+table.rect = table.image.get_rect()
+table.rect = [200, 527]
+all_sprites.add(table)
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('Уровень 1. Спальня')
@@ -175,7 +210,7 @@ if __name__ == '__main__':
                 sprite.rect = hero.moving_down(sprite.rect[0], sprite.rect[1])
             else:
                 hero.drawing()
-            screen.fill((0, 0, 0))
+            screen.fill((255, 160, 122))
             board.render(screen)
             all_sprites.draw(screen)
             pygame.display.flip()
