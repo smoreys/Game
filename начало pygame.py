af559e6ddf38a78018e1c63222d325261c0dbf34
smoreys <150430670+smoreys@users.noreply.github.com>
2024-01-07 20:48:49 +0700

    Update начало pygame.py
    Сделала, чтобы можно было заходить в игру

--- начало pygame.py
+++ начало pygame.py
@@ -1,5 +1,8 @@
 import pygame
 import random
+import sys
+import os
+from main import runbedroom
 
 a = 0
 b = 0
@@ -16,33 +19,26 @@ def draw(screen):
     text_h = text.get_height()
     screen.blit(text, (text_x, text_y))
     pygame.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10, text_w + 20, text_h + 20), 1)
-    text1 = font.render("запустить", True, (pygame.Color('white')))
-    screen.blit(text1, (text_x+10, text_y + 100))
     for i in range(10000):
         screen.fill(pygame.Color('white'),
                     (random.random() * width,
                      random.random() * height, 1, 1))
 
 
-
-
 if __name__ == '__main__':
-    # инициализация Pygame:
     pygame.init()
-    # размеры окна:
-    size = width, height = 800, 600
-    # screen — холст, на котором нужно рисовать:
+    size = width, height = 800, 800
     screen = pygame.display.set_mode(size)
     draw(screen)
-    # формирование кадра:
-    # команды рисования на холсте
-    # ...
-    # ...
-    # смена (отрисовка) кадра:
     pygame.display.flip()
-
-    # ожидание закрытия окна:
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
-    # завершение работы:
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                print('bebe')
+                runbedroom(True)
+            else:
+                draw(screen)
     pygame.quit()
